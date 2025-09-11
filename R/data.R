e59a715b915a950fd79f033f332cd42bc6bb851f
xuewei cao <36172337+xueweic@users.noreply.github.com>
2025-09-11 14:36:45 -0400

    upload example data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Visium human DLPFC data
+#'
+#' An example dataset
+#'
+#' @format ## `Visium_human_DLPFC`
+#' A list with 3 elements
+#' \describe{
+#'   \item{counts}{****}
+#'   \item{spatial_coords}{****}
+#'   \item{cell_type_proportions}{****}
+#' }
+#' @source The Visium_human_DLPFC dataset contains
+#'
+#' @family spacelink_data
+"Visium_human_DLPFC"
