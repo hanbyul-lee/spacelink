38d4c4d103c666c82c0e914c1ced5e892800e9df
Hanbyul Lee <lhbjully@gmail.com>
2025-09-11 20:59:08 -0400

    Update data.R

--- R/data.R
+++ R/data.R
@@ -5,9 +5,9 @@
 #' @format ## `Visium_human_DLPFC`
 #' A list with 3 elements
 #' \describe{
-#'   \item{counts}{****}
-#'   \item{spatial_coords}{****}
-#'   \item{cell_type_proportions}{****}
+#'   \item{counts}{Gene expression count matrix. Rows and columns indicate genes and spots, respectively.}
+#'   \item{spatial_coords}{Spatial coordinates matrix. Rows indicate spots.}
+#'   \item{cell_type_proportions}{Cell type proportion matrix estimated by RCTD. Rows and columns indicate spots and cell types, respectively.}
 #' }
 #' @source The Visium_human_DLPFC dataset contains
 "Visium_human_DLPFC"
